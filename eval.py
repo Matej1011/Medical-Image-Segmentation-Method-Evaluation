9fc00724f50ac9aa5a91965d4ba94b09c7b7851d
Matej1011 <63561953+Matej1011@users.noreply.github.com>
2024-07-26 12:41:05 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,3 +1,5 @@
+#I've removed comments that I had before which were just code I used to troubleshoot/debug
+#It was mainly print statements that told me things like data shape, max/min values, and patient numbers
 
 import nibabel as nib
 import numpy as np
@@ -12,84 +14,70 @@ def search_files(directory, pattern):
     return matches
 
 dir = r'C:\Users\mijan\Desktop\Summer Research\rsna-cspine-segmentations\nifti_source_files_segmentations'
-compressed_niftis = search_files(dir, '*nii.gz')
+TS_masks = search_files(dir, '*nii.gz')
 
 dir = r'C:\Users\mijan\Desktop\Summer Research\rsna-cspine-segmentations'
-files = search_files(dir, '*.nii')
+GT_files = search_files(dir, '*.nii')
 
-sum = 0
+
+
 total_iou = 0
+segment_IOU = 0
 
-#this loops through the masks/segmentations from totalSegmentator
-for segmentations in compressed_niftis:
-    sum = sum+1
-print('Segmentations: ')
-print(sum)
+def calculate_IOU(ground_truth_data, mask_directory, i):
 
-sum = 0
-#this loops through the ground truths
-for niftis in files:
-    sum=sum+1
-print('Ground truths: ')
-print(sum)
-
-def calculate_IOU(ground_truth_directory, mask_directory, segmentation_num):
-
-    # Load the NIfTI files
-    #mask 1 is ground truth
-    
-    ground_truth_img = nib.load(ground_truth_directory)
+    # Load the NIfTI file for the mask
     mask_img = nib.load(mask_directory)
-
-    # Get the data from the NIfTI images
-    ground_truth_data = ground_truth_img.get_fdata()
     mask_data = mask_img.get_fdata()
 
-    #print(f"Shape of mask1: {ground_truth_data.shape}")
-    #print(f"Shape of mask2: {mask_data.shape}")
 
-    # Ensure the masks are binary (values 0 or 1)
-    ground_truth_data = (ground_truth_data > 0).astype(int)
+    ground_truth_data = (ground_truth_data == i).astype(int)
     mask_data = (mask_data > 0).astype(int)
 
     # Calculate the intersection of the masks
-    intersection_data = np.logical_and(ground_truth_data, mask_data).astype(int)
-    union_data = np.logical_or(ground_truth_data, mask_data).astype(int)
+    intersection_data = np.logical_and(ground_truth_data, mask_data)
+    union_data = np.logical_or(ground_truth_data, mask_data)
+
 
     intersect_num = np.sum(intersection_data)
     union_num = np.sum(union_data)
-    if union_num != 0:
-        iou_num = intersect_num / union_num * 100
-    else:
-        iou_num = 0
-    
-    #print(mask1_data.max())
-    #print(mask1_data.sum())
-    #print(mask2_data.max())
-    #print(mask2_data.sum())
 
-    #print(f"Intersection number = {intersect_num}")
-    #print(f"Union number = {union_num}")
-    #print(f"IOU (%): {iou_num}")
-    
-    print("Mask #: ", seg_num)
+    assert union_num != 0, "Union_num is zero for patient ID"
+
+    iou_num = intersect_num / union_num
+
 
     return iou_num
 
+mask_index = 1
+patient_num = 0
+iou = 0
 
-#iterate through the list of segmentations and add to the total IOU score (which will probably have to be changed)
-GT_index = -1
-mask_index = 0
-seg_num = 1
-patient_num = 1
+#list to hold the averages of each segment's IOU
+segment_averages = [0, 0, 0, 0, 0, 0, 0, 0]
 
-for gt in files:
-    GT_index += 1
-    mask_index +=1
-    print("IOU: ", total_iou)
-    print("Patient #: ", patient_num)
-    patient_num += 1
+for gt in GT_files:
+
+    ground_truth_img = nib.load(gt)
+    ground_truth_data = ground_truth_img.get_fdata()
+
+    #after loading one of the ground truth files, compare it with masks for C1 to T1 (8 total) and track value for overall IOU and for each segment
     for i in range(8):
-        total_iou += calculate_IOU(files[GT_index], compressed_niftis[mask_index], seg_num)
-        mask_index +=1
-        seg_num += 1
+        segment_IOU = calculate_IOU(ground_truth_data, TS_masks[mask_index], i+1)
+        total_iou += segment_IOU
+        segment_averages[i] += segment_IOU
+        mask_index += 1
+
+    mask_index += 1
+    patient_num += 1
+    iou = 0
+    print("Files processed: ", patient_num)
+
+#Rest is just printing out results
+print("Average of all IOUs: ", (total_iou / 87 / 8))
+
+for i in range(8):
+    if i < 7:
+        print("IOU average for C", i+1, ": ", segment_averages[i] / 87)
+    else:
+        print("IOU average for T1: ", segment_averages[7] / 87)
